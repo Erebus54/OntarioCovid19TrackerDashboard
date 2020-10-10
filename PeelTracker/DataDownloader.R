a9965386ef73bc24e5b057e4191401cec66033d8
Patrick Schnurbusch <30598863+Erebus54@users.noreply.github.com>
2020-10-10 19:19:52 -0400

    Add files via upload

---
+++ PeelTracker/DataDownloader.R
@@ -0,0 +1,35 @@
+library(lubridate)
+library(data.table)
+
+setwd("C:/Users/patri/Documents/covid19/TrackerDashboards/TrackerDashboards/PeelTracker/")
+
+#grab file 
+casestatus <- read.csv(file = "https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv")
+
+#data dict URL 
+#https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario
+#end_time <- Sys.time()
+#end_time - start_time
+
+
+#clean CaseStatus file
+casestatus$Accurate_Episode_Date <- as.Date(casestatus$Accurate_Episode_Date)
+casestatus$Case_Reported_Date <- as.Date(casestatus$Case_Reported_Date)
+casestatus$Test_Reported_Date <- as.Date(casestatus$Test_Reported_Date)
+
+target <- "Peel Public Health"
+
+casestatus <- casestatus %>% 
+  dplyr::filter(Reporting_PHU == target)
+
+#create .csv for each date of dataset
+dirPath <- "C:/Users/patri/Documents/covid19/TrackerDashboards/TrackerDashboards/PeelTracker/datasets/"
+#Delete files if exists 
+do.call(file.remove, list(list.files(dirPath, full.names = TRUE)))
+fileName = paste(dirPath, 'casestatus.csv',sep = '')
+#Write Case Status File 
+write.csv(casestatus, 
+          file = fileName, 
+          fileEncoding = 'UTF-8', 
+          row.names = F)
+
